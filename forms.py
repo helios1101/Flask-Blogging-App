af3c37d129d1227ba6dd60022f445e657260a42b
helios1101 <harshit.singh1101@gmail.com>
2018-11-20 02:20:25 +0530

    Error_in_display_data

--- forms.py
+++ forms.py
@@ -4,21 +4,10 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-<<<<<<< HEAD
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
-=======
-    username = StringField(
-        'Username', validators=[DataRequired(),
-                                Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField(
-        'Confirm Password', validators=[DataRequired(),
-                                        EqualTo('password')])
->>>>>>> 4a05232c26ddb372dd113f9a66e6e97b04f6a8fb
     submit = SubmitField('Sign Up')
 
 
