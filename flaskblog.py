1f0cb721a924861a2c69b1486354cf8f99b5a22a
helios1101 <harshit.singh1101@gmail.com>
2018-11-20 02:04:22 +0530

    Error_in_display_data

--- flaskblog.py
+++ flaskblog.py
@@ -13,39 +13,39 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(
-        db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return "User('{self.username}', '{self.email}', '{self.image_file}')"
+        return `User('${self.username}', '${self.email}', '${self.image_file}')`
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(
-        db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return "Post('{self.title}', '{self.date_posted}')"
+        return "Post(`${self.title}`, `${self.date_posted}`)"
 
 
-posts = [{
-    'author': 'Corey Schafer',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'April 20, 2018'
-},
-         {
-             'author': 'Jane Doe',
-             'title': 'Blog Post 2',
-             'content': 'Second post content',
-             'date_posted': 'April 21, 2018'
-         }]
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route("/")
@@ -76,8 +76,7 @@ def login():
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password',
-                  'danger')
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
