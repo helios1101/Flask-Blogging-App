cd4b617d02cc48839baab49080fcc56af8776696
Harshit Singh <harshit.singh1101@gmail.com>
2018-12-04 01:50:59 +0530

    Updated

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -4,11 +4,9 @@ from flask import current_app
 from flaskblog import db, login_manager
 from flask_login import UserMixin
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,7 +31,6 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
